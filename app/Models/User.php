bf6ed793518674272fc0a23ad7d375e930b947ca
RishikantSri <rishi.kant.sri@gmail.com>
2023-12-27 10:09:57 +0530

    Schedules Notification module completed, saving to DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\ScheduledNotification;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class User extends Authenticatable
 {
